6137ae54a5179fb21ab70fe402fca2b7e6a4cebc
dogyeong <shroad1802@gmail.com>
2025-02-15 15:53:52 +0900

    feat: 이벤트 목록을 최신순으로 정렬

--- pages/popup/src/Events.tsx
+++ pages/popup/src/Events.tsx
@@ -13,25 +13,27 @@ export const Events = ({ events, onClickEvent, selectedEventId }: EventsProps) =
 
   return (
     <ul className="">
-      {events.map(({ id, date, data }) => (
-        <li key={id} className="[&:not(:last-of-type)]:border-b-zinc-600 [&:not(:last-of-type)]:border-b-[0.5px]">
-          <button
-            type="button"
-            className={
-              'p-[16px] cursor-pointer flex gap-x-[12px] items-center w-full' +
-              ' ' +
-              (selectedEventId === id ? 'bg-zinc-700' : '')
-            }
-            onClick={() => onClickEvent(id)}>
-            <time dateTime={date.toLocaleDateString()} className="text-zinc-400 shrink-0 text-[12px]">
-              {date.toLocaleTimeString()}
-            </time>
-            <p className="text-zinc-100 break-all text-pretty text-[16px] text-start">
-              {String(data.event.amplitude.event_type)}
-            </p>
-          </button>
-        </li>
-      ))}
+      {events
+        .sort((a, b) => b.data.date - a.data.date)
+        .map(({ id, date, data }) => (
+          <li key={id} className="[&:not(:last-of-type)]:border-b-zinc-600 [&:not(:last-of-type)]:border-b-[0.5px]">
+            <button
+              type="button"
+              className={
+                'p-[16px] cursor-pointer flex gap-x-[12px] items-center w-full' +
+                ' ' +
+                (selectedEventId === id ? 'bg-zinc-700' : '')
+              }
+              onClick={() => onClickEvent(id)}>
+              <time dateTime={date.toLocaleDateString()} className="text-zinc-400 shrink-0 text-[12px]">
+                {date.toLocaleTimeString()}
+              </time>
+              <p className="text-zinc-100 break-all text-pretty text-[16px] text-start">
+                {String(data.event.amplitude.event_type)}
+              </p>
+            </button>
+          </li>
+        ))}
     </ul>
   );
 };
